40d6eb73df774a8ebe0912af928ae5129186789f
Aquerr <nerdianin@gmail.com>
2018-10-04 10:03:39 +0200

    Updated main window logic

--- Ships/MainWindow.xaml.cs
+++ Ships/MainWindow.xaml.cs
@@ -20,10 +20,11 @@ namespace Ships
     /// </summary>
     public partial class MainWindow : Window
     {
-        string[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
-        Dictionary<string, Button> playerGridFields;
-        GameState gamePhase = GameState.OFF;
+        private readonly string[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 
+        private Dictionary<string, Button> playerGridFields;
+        private Dictionary<string, Button> enemyGridFields;
+        private GameState gamePhase = GameState.OFF;
 
         public MainWindow()
         {
@@ -35,8 +36,7 @@ namespace Ships
             playerGridFields = new Dictionary<string, Button>();
             gamePhase = GameState.PREPARATION;
 
-            //Load rectangles in player grid
-
+            //Load player grid
             for (int i = 1; i <= 11; i++)
             {
                 for (int j = 1; j <= 11; j++)
@@ -79,47 +79,103 @@ namespace Ships
                     }
                 }
             }
+
+            //Load enemy grid
+            for (int i = 1; i <= 11; i++)
+            {
+                for (int j = 1; j <= 11; j++)
+                {
+                    if (i == 1)
+                    {
+                        if (j != 1)
+                        {
+                            Label label = new Label();
+                            label.Name = "enemyLbl_" + letters[j - 2];
+                            label.Content = letters[j - 2];
+                            label.SetValue(Grid.ColumnProperty, j - 1);
+                            label.SetValue(Grid.RowProperty, i - 1);
+                            PlayerShootingGrid.Children.Add(label);
+                        }
+                    }
+                    else
+                    {
+                        if (j == 1)
+                        {
+                            Label label = new Label();
+                            label.Name = "enemyLbl_" + (i - 1);
+                            label.Content = i - 1;
+                            label.SetValue(Grid.ColumnProperty, j - 1);
+                            label.SetValue(Grid.RowProperty, i - 1);
+                            PlayerShootingGrid.Children.Add(label);
+                        }
+                        else
+                        {
+                            Button button = new Button();
+                            button.Name = "enemyBtn_" + letters[j - 2] + i;
+                            button.Background = Brushes.White;
+                            button.FontSize = 18;
+                            button.SetValue(Grid.ColumnProperty, j - 1);
+                            button.SetValue(Grid.RowProperty, i - 1);
+                            button.Click += new RoutedEventHandler(EnemyGridClick);
+                            PlayerShootingGrid.Children.Add(button);
+                            enemyGridFields.Add(letters[j - 2] + i, button);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void EnemyGridClick(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void PlayerGridClick(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-
-            if (gamePhase == GameState.PREPARATION)
+            switch (gamePhase)
             {
-                if(button.Background == Brushes.White)
+                case GameState.PREPARATION:
+                    handlePrepareGridClick(button);
+                    break;
+                case GameState.RUNNING:
+                    handleRunningGridClick(button);
+                    break;
+                case GameState.OFF:
+                    //handleOFFGridClick(button);
+                    break;
+
+                default:
+                    MessageBox.Show("Invalid game state! Please restart your game!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+            }
+        }
+
+        private void handleRunningGridClick(Button button)
+        {
+            //Check if button is a ship
+            if (button.Background == Brushes.Gray)
+            {
+                if (button.Content == null || button.Content.Equals(""))
                 {
-                    button.Background = Brushes.Gray;
+                    button.Content = "X";
                 }
                 else
                 {
-                    button.Background = Brushes.White;
+                    button.Content = "";
                 }
             }
-            else if(gamePhase == GameState.RUNNING)
+        }
+
+        private void handlePrepareGridClick(Button button)
+        {
+            if (button.Background == Brushes.White)
             {
-                if (button.Name.StartsWith("player"))
-                {
-                    if (button.Content.Equals(""))
-                    {
-                        button.Content = "X";
-                    }
-                    else
-                    {
-                        button.Content = "";
-                    }
-                }
-                else
-                {
-                    if (button.Name.Equals(""))
-                    {
-                        button.Content = "●";
-                    }
-                    else
-                    {
-                        button.Content = "";
-                    }
-                }
+                button.Background = Brushes.Gray;
+            }
+            else
+            {
+                button.Background = Brushes.White;
             }
         }
 
